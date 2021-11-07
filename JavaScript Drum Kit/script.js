1d8cfebd7251942d9fba84fd444d87d5eebe718f
Dmitry <donveds@gmail.com>
2021-11-07 19:44:43 +0300

    move eventlistener to bottom

--- JavaScript Drum Kit/script.js
+++ JavaScript Drum Kit/script.js
@@ -1,5 +1,4 @@
-window.addEventListener('keydown', function(e){
-
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return;
@@ -9,14 +8,16 @@ window.addEventListener('keydown', function(e){
   audio.play();
 
   key.classList.add("playing");
-});
+};
 
 function removeTransition(e){
   if (e.propertyName !== "transform") return;
   console.log(e.propertyName);
   
   this.classList.remove("playing");
-}
+};
 
 const keys = document.querySelectorAll(".key");
 keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener('keydown', playSound);
